5348a92d2344d442fe36e3ab05ad92de7f027c47
Prat15072000 <pratsimple2000@gmail.com>
2020-07-31 21:47:28 +0530

    Create main.dart
    Relocate the home in material app widget

---
+++ ample/lib/main.dart
@@ -0,0 +1,24 @@
+//import 'package:ample/forgotPass7.dart';
+import 'package:ample/gridapi.dart';
+import 'package:ample/listView.dart';
+import 'package:ample/numberClass.dart';
+import 'package:flutter/material.dart';
+import 'list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TicketPage(),
+    );
+
+  }
+}
+
+
+
